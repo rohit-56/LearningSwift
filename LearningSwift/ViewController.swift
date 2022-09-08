718e3183edb028feda3ebc906385a8dd44748ad0
rohit-56 <rohitmou25@gmail.com>
2022-09-08 12:47:01 +0530

    implement class & structure

--- LearningSwift/ViewController.swift
+++ LearningSwift/ViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    class myClass{
+        var name:String
+        
+        init(name:String){
+            self.name=name
+        }
+    }
+    
+    struct myStruct{
+        var name : String
+        
+        init(name : String){
+            self.name=name
+        }
+    }
+    
+    func callClass(){
+        let fname=myClass(name: "Rohit")
+        let sname=fname
+        
+        sname.name="Sharma"
+        
+        print(fname.name)
+        print(sname.name)
+    }
+    func callStruct(){
+        let fname=myStruct(name: "Rohit")
+        
+        var sname=fname
+        sname.name="Sharma"
+        
+        print("fname in struct \(fname.name)")
+        print("sname in strcut \(sname.name)")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        callClass()
+        callStruct()
     }
 
 
